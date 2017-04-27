1ac6a26bd8c7d6336d2b934841b3c9622c64c8d8
TingYao Hsu <tingyao.hsu@gmail.com>
2017-04-27 23:28:55 +0800

    Create project

---
+++ LightWonder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LightWonder
+//
+//  Created by 許庭耀 on 2017/4/27.
+//  Copyright © 2017年 許庭耀. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
