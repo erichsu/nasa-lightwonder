356be13bdae760f79f90872bfe51f64e349e7f15
TingYao Hsu <tingyao.hsu@gmail.com>
2017-04-29 10:51:00 +0800

    Add UI layouts

--- LightWonder/ViewController.swift
+++ LightWonder/ViewController.swift
@@ -7,12 +7,65 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+
+import Kingfisher
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var waveLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        
+        locationManager.distanceFilter = kCLLocationAccuracyNearestTenMeters
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        let url = URL(string: "https://placehold.it/150x100")
+        weatherImage.kf.setImage(with: url)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // 首次使用 向使用者詢問定位自身位置權限
+        if CLLocationManager.authorizationStatus() == .notDetermined {
+            // 取得定位服務授權
+            locationManager.requestWhenInUseAuthorization()
+            
+            // 開始定位自身位置
+            locationManager.startUpdatingLocation()
+        } else if CLLocationManager.authorizationStatus() == .denied {
+            // 提示可至[設定]中開啟權限
+            let alertController = UIAlertController(
+                title: "定位權限已關閉",
+                message: "如要變更權限，請至 設定 > 隱私權 > 定位服務 開啟",
+                preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "確認", style: .default, handler:nil)
+            alertController.addAction(okAction)
+            self.present(alertController, animated: true, completion: nil)
+        } else if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            // 開始定位自身位置
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        // 停止定位自身位置
+        locationManager.stopUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +76,12 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //
+        let currentLocation :CLLocation =
+            locations[0] as CLLocation
+        print("\(currentLocation.coordinate.latitude)")
+        print(", \(currentLocation.coordinate.longitude)")
+    }
+}
